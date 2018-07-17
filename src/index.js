802b37b8814da062eabf3995b0b9c5e211d88936
Sheraz Ali <sheraz.ali342@gmail.com>
2018-07-17 19:33:18 +0500

    added video details and embeded player with responsive iframe

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './compnents/search_bar';
 import VideoList from './compnents/video_list';
+import VideoDetail from './compnents/video_detail';
+
 import YoutubeVideoSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBfTwfXGOcrMUNaK8TrdfbdcWC_qsPcedQ';
@@ -10,16 +12,17 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [], selectedVideo: null };
 
     YoutubeVideoSearch({ key: API_KEY, term: 'Best Song Ever' }, videos => {
-      this.setState({ videos });
+      this.setState({ videos, selectedVideo: videos[0] });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
